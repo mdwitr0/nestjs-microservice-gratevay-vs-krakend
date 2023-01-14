ea34d11845e24a3eed2644ed9242aaf007fce410
mdwitr0 <mdwit0r@gmail.com>
2023-01-14 22:49:45 +0100

    generate dto

---
+++ libs/prisma-clients/inventory/src/generated/dto/create-product.dto.ts
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+export class CreateProductDto {
+  name: string;
+price: number;
+creatorId: string;
+deletedAt?: Date;
+}
---
+++ libs/prisma-clients/inventory/src/generated/dto/product.entity.ts
@@ -0,0 +1,14 @@
+
+
+
+
+export class Product {
+  id: string ;
+uuid: string ;
+name: string ;
+price: number ;
+creatorId: string ;
+createdAt: Date ;
+updatedAt: Date ;
+deletedAt: Date  | null;
+}
