1d1717a5dab18381546a85f933357baf2146c8b7
mdwitr0 <mdwit0r@gmail.com>
2023-01-15 13:32:58 +0100

    add suffix to entity & export entities from lib

--- libs/prisma-clients/inventory/src/index.ts
+++ libs/prisma-clients/inventory/src/index.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
+
 export * from './lib/prisma-inventory.module';
 export * from './lib/prisma-inventory.service';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 export { PrismaClient as InventoryClient } from '.prisma/inventory-client';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 export * from '.prisma/inventory-client';
+export * from '.prisma/inventory-dto';
--- libs/prisma-clients/user/src/index.ts
+++ libs/prisma-clients/user/src/index.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
+
 export * from './lib/prisma-user.module';
 export * from './lib/prisma-user.service';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 export { PrismaClient as InventoryClient } from '.prisma/user-client';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 export * from '.prisma/user-client';
+export * from '.prisma/user-dto';
