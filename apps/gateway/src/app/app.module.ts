046c7d5b21768d1272d5f2ca888abfc4cb8c04e3
mdwitr0 <mdwit0r@gmail.com>
2023-01-15 21:09:01 +0100

    import client to gateway

--- apps/gateway/src/app/app.module.ts
+++ apps/gateway/src/app/app.module.ts
@@ -2,9 +2,39 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ServiceQueues, Services } from '@enum';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: Services.USER,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URI')],
+            queue: configService.get(ServiceQueues.USER),
+          },
+        }),
+      },
+      {
+        name: Services.INVENTORY,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URI')],
+            queue: configService.get(ServiceQueues.INVENTORY),
+          },
+        }),
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
