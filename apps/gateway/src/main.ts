8e7691d3dc29e309456e31e0dfbaa562893881b3
mdwitr0 <mdwit0r@gmail.com>
2023-01-15 22:13:25 +0100

    add swagger

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -7,12 +7,19 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ProductEntity } from '@dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  const options = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, options, { extraModels: [ProductEntity] });
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
